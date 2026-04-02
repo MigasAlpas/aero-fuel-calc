9dd66bdf1ed32abbf535d65316b91b312bd06173
MigasAlpas <miguel.m.alpalhao@gmail.com>
2026-04-02 11:54:39 +0100

    Update environment.ts

--- environments/environment.ts
+++ environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   googleApiKey: 'AIzaSyC9ZVQiDOouaKFnkmO945CMXPiEkiptKYk',
   spreadsheetId: '1p8GcM6X-hwItl4XKJUIUTKlCOpjQ5jflp3TOFn4Yhn8',
-  pw: 'TheTimeIsNow!25'
+  pw: 'TheTimeIsNow!26'
 };
